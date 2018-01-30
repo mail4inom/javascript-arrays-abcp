4ba418846a292bc2b7d406a8f1b9d710a2a68166
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-30 04:10:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,9 @@ function destructivelyAddElementToBeginningOfArray(arr, element){
  arr.unshift(element)
  return arr;
 }
+function addElementToEndOfArray(arr, element){
+  return [...arr, element]
+} 
+function destructivelyAddElementToEndOfArray(arr, element){
+  
+}
