8ea4d366b155545bbe4d40d8f20323dfadc264dc
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-30 03:57:02 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
 
 function addElementToBeginningOfArray(arr, element){
-  return arr.unshift(element);
+  return arr.unshift("element");
 }
 function destructivelyAddElementToBeginningOfArray(arr, element){
 
